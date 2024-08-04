1439829df4bb6e12ee804244282b70b8507600da
GhostDev <gillomdev@gmail.com>
2024-08-03 23:52:58 -0700

    Advance 1.1 03-08

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,18 @@
-function openNav(){
-    document.getElementById("mobile-menu").style.width = "100%";
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
 }
 
-function closeNav(){
-    document.getElementById("mobile-menu").style.width = "0%";
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
 }
